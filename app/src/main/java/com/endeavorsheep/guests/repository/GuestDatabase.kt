f35880a379297622a9386e22e0c0540f042dd9f6
higor-jugler <100137983+higor-jugler@users.noreply.github.com>
2023-11-23 13:42:49 -0300

    Segunda parte do room implementado

--- app/src/main/java/com/endeavorsheep/guests/repository/GuestDatabase.kt
+++ app/src/main/java/com/endeavorsheep/guests/repository/GuestDatabase.kt
@@ -3,18 +3,33 @@ package com.endeavorsheep.guests.repository
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.endeavorsheep.guests.constants.DatabaseConstants
+import com.endeavorsheep.guests.model.GuestModel
 
-class GuestDatabase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
-
+@Database(entities = [GuestModel::class], version = 1)
+abstract class GuestDatabase() : RoomDatabase() {
     companion object {
-        private const val NAME = "guestDb"
-        private const val VERSION = 1
-    }
-
-    override fun onCreate(db: SQLiteDatabase) { }
-
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+        private lateinit var INSTANCE: GuestDatabase
+        fun getDatabase(context: Context): GuestDatabase {
+            if (!::INSTANCE.isInitialized) {
+                synchronized(GuestDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(context, GuestDatabase::class.java, "guestdb")
+                        .addMigrations(MIGRATION)
+                        .allowMainThreadQueries()
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+        private val MIGRATION: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("DELETE FROM Guest")
+            }
+        }
     }
 }
